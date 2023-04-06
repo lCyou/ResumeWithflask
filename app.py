3031a12340740f17d76fda789be6e05bf60c8da4
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2023-04-06 22:12:42 +0900

    Add: head sorce added
    DOMのheadの部分を見よう見まねで書いた

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template("resume.html")
+
