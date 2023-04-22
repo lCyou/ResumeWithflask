f9ffa32b94e3ffad9781d10db80238b4bd119ceb
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2023-04-22 18:52:45 +0900

    replace and rename files

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, Blueprint
 
-app = Flask(__name__)
+app = Flask(__name__,
+                    static_folder='static',
+                    template_folder='templates')
 
 @app.route('/')
-def home():
+def index():
     return render_template("resume.html")
+
+if __name__ == '__main__':
+    app.run()
 
